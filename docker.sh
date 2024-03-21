29ccd7f970c1001626c3acffc6c683ae5a77d4d7
Fire <17692105+FireMario211@users.noreply.github.com>
2024-03-21 20:28:48 +0000

    make sure it doesnt just stop

--- docker.sh
+++ docker.sh
@@ -2,4 +2,4 @@
 sudo docker build -t neffbox .
 sudo docker stop neffbox
 sudo docker rm neffbox
-sudo docker run -d -p 127.0.0.1:10080:10080 --name neffbox neffbox
+sudo docker run -d -p 127.0.0.1:10080:10080 --restart unless-stopped --name neffbox neffbox
